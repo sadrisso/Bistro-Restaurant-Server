8a5742bb7f6c11ddbd4545ba5eb60925ed0073fa
sadrisso <sadriisso304@gmail.com>
2024-12-31 21:36:06 +0600

    created get apis for menu and reviews data

--- index.js
+++ index.js
@@ -29,8 +29,19 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const menuCollection = client.db("bistroDB").collection("menu")
+        const reviewCollection = client.db("bistroDB").collection("reviews")
 
 
+        app.get("/menu", async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get("/reviews", async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
