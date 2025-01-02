7a380642af39db6f6f472c670c448c4908778857
sadrisso <sadriisso304@gmail.com>
2025-01-02 19:26:16 +0600

    specific id deleted from the database

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -46,7 +46,9 @@ async function run() {
 
 
         app.get("/cartItems", async (req, res) => {
-            const result = await cartCollection.find().toArray()
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray()
             res.send(result)
         })
 
@@ -54,6 +56,14 @@ async function run() {
         app.post("/cartItems", async (req, res) => {
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+
+
+        app.delete("/cartItems/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
 
